f037412202104769ce4e6cf46f892cdd5a92fcee
Jingming <liujingming517@gmail.com>
2020-03-30 17:20:53 +0800

    game maze (work)

--- SoundDisplay.srcs/sources_1/new/FU_Indicator.v
+++ SoundDisplay.srcs/sources_1/new/FU_Indicator.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: laozinmdxintaibengle
-// 
-// Create Date: 2020/03/27 00:33:54
-// Design Name: 
-// Module Name: FU_Indicator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 module AVL_Indicator(
     input DCLK,   //100Mhz
     input RefSCLK,  //20k
