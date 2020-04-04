d3ee3fdb24766feb1343226bdb287e1132d8d465
Jingming <liujingming517@gmail.com>
2020-04-04 16:12:41 +0800

    FFT version 2.0 finished 1000Hz

--- SoundDisplay.srcs/sources_1/new/Audio_FFT_discrete.v
+++ SoundDisplay.srcs/sources_1/new/Audio_FFT_discrete.v
@@ -26,6 +26,7 @@ module Audio_FFT_discrete(input [11:0] mic_in,CLK,MCLK,output reg [2:0]FREQ);
     reg [13:0] c; //counter variable for sampling
     reg [13:0] counter = 0;
     wire [2:0]FFTCLK;
+    reg [4:0] vart_1000 = 5'b00001;  //1~20
     FFT_clock clk(MCLK, FFTCLK); 
     /* 
     Part 1: Volume Sampling
@@ -48,14 +49,73 @@ module Audio_FFT_discrete(input [11:0] mic_in,CLK,MCLK,output reg [2:0]FREQ);
         else begin
             counter <= counter + 1;    
             SAMPLE[counter] = mic_in;   //g(t)
+            if(vart_1000==5'd20) begin
+                if(final_cal_1000 == 2'b10) begin    //reset after finish all calculations
+                    vart_1000<= 5'b00001;
+                end
+            end
+            else vart_1000 <= vart_1000 + 1'b1;
         end
     end
     
-    
     /*
     Part 2: FFT formula inplementation
-    for now we have three frequencies, 1000Hz, 2000Hz, 3000Hz
-    
+    for now we have three frequencies, 1000Hz, 2000Hz, 3000Hz,
     */
+    reg [28:0] RESULT_1000;
+    reg [28:0] REAL_1000;
+    reg [28:0] REAL_pos_1000;
+    reg [28:0] REAL_neg_1000;
+    reg [28:0] IMAG_1000;
+    reg [28:0] IMAG_pos_1000;
+    reg [28:0] IMAG_neg_1000;
+    reg [8:0] angle_1000;   //0~360
+    reg [8:0] angle_180_1000;
+    reg [6:0] angle_90_1000;
+    //(7'b0 ~7'b1011010) (0~90)
     
+    reg [12:0] SIN_value_1000;
+    reg [12:0] COS_value_1000;
+    FFT_sin sin_1000(angle_90_1000, SIN_value_1000);
+    FFT_cos cos_1000(angle_90_1000, COS_value_1000);
+    
+    reg [1:0] final_cal_1000 = 2'b00;
+    always @(posedge MCLK) begin
+        //angle = (13'd6280 * vart_1000 / 15'd20000) * 9'd360 / 6.28;  // (0~6.28) need to improve the calculation
+        angle_1000 = 19'd360000 * vart_1000; //(0~360)
+        
+        if (angle_1000 < 8'd180) begin   //final value is positive
+            if (angle_1000 <= 7'd90) begin
+                angle_90_1000 = angle_1000;
+                IMAG_pos_1000 <=  IMAG_pos_1000 + mic_in * SIN_value_1000;
+            end
+            else begin
+                angle_90_1000 = angle_1000 - 7'd90; //sin£¨¦Ð/2£«¦Á£©£½cos¦Á
+                IMAG_pos_1000 <= IMAG_pos_1000 + mic_in * COS_value_1000;
+            end
+        end
+        else begin //final value is negative  //sin£¨¦Ð£«¦Á£©£½£­sin¦Á
+            angle_180_1000 = angle_1000 - 9'd180;
+            if (angle_180_1000 <= 7'd90) begin
+                angle_90_1000 = angle_1000;
+                IMAG_neg_1000 <=  IMAG_neg_1000 + mic_in * SIN_value_1000;
+            end
+            else begin
+                angle_90_1000 = angle_1000 - 7'd90; //sin£¨¦Ð/2£«¦Á£©£½cos¦Á
+                IMAG_neg_1000 <= IMAG_neg_1000 + mic_in * COS_value_1000;
+          end
+        end
+        
+        if(vart_1000==5'd20) begin
+            final_cal_1000 = final_cal_1000 + 1;
+            REAL_1000 = REAL_pos_1000 - REAL_neg_1000;
+            IMAG_1000 = IMAG_pos_1000 - IMAG_neg_1000;
+        end
+        if (final_cal_1000 == 2'b01) begin
+            RESULT_1000 = IMAG_1000*IMAG_1000 + REAL_1000*REAL_1000;
+            final_cal_1000 = final_cal_1000 + 1;
+        end
+        if (final_cal_1000 -- 2'b10) final_cal_1000 <= 2'b00;
+    end
+
 endmodule
