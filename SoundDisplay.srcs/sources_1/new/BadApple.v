f436b94ef2bb3edbf9cc88879f578717923fbb73
Li Bozhao <libozhao142857@hotmail.com>
2020-04-05 10:55:21 +0800

    BAD APPLE ALMOST WORKING

--- SoundDisplay.srcs/sources_1/new/BadApple.v
+++ SoundDisplay.srcs/sources_1/new/BadApple.v
@@ -15,14 +15,14 @@
 // Additional Comments: Developed by Li Bozhao, all rights reserved.
 //////////////////////////////////////////////////////////////////////////////////
 module PixelFromPos(input [7:0] Data, input [12:0] PosOnFrame, output [6:0] X1, output [6:0] X2, output [5:0] Y, output [15:0] C, output [6:0] LEN);
-    wire [6:0] cX = PosOnFrame / 7'd64;
-    wire [6:0] cY = PosOnFrame - 7'd64 * cX;
+    wire [6:0] cX = (PosOnFrame + 1'b1) / 7'd64;
+    wire [6:0] cY = (PosOnFrame + 1'b1) - 7'd64 * cX;
     wire [6:0] cX1 = cX << 1'd1;//cx * 2
     wire [6:0] cX2 = cX1 + 1'd1;
     assign X1 = cX1;
     assign X2 = cX2;
     assign Y = cY;
-    assign C = Data[7] ? 16'd0 : 16'd65535;
+    assign C = Data[7] ? 16'd65535 : 16'd0;
     assign LEN = Data[6:0];
 endmodule
 
@@ -36,8 +36,8 @@ module BadAppleCore(input CLK, input ON, input PAUSE, input Clk10Hz, output Writ
     reg [12:0] PosOnFrame = 13'b0;
     reg [12:0] PixelCounter = 13'b0;
     PixelFromPos PFP(Data, PosOnFrame, x1, x2, Y, C, LEN);
-    reg [1:0] vDup = 2'd0;
-    assign X = vDup[1] ? x1 : x2;
+    reg [2:0] vDup = 3'd0;
+    assign X = vDup[2] ? x1 : x2;
     reg [12:0] StartPos = 0;
     wire [12:0] PosUBound = StartPos + LEN;
     wire DONEFRAME = PixelCounter >= 12'd3072;
@@ -53,13 +53,14 @@ module BadAppleCore(input CLK, input ON, input PAUSE, input Clk10Hz, output Writ
                 PosOnFrame = 1'b0;
                 PixelCounter = 1'b0;
                 StartPos = 0;
+                vDup = 3'd0;
             end
             if (Write) begin
                 vDup = vDup + 1;
-                if (vDup == 2'b00) begin
+                if (vDup == 3'b000) begin
                     PixelCounter = PixelCounter + 1;
                     if (PixelCounter < 12'd3072) PosOnFrame = PixelCounter;
-                    if (PosOnFrame == PosUBound) begin 
+                    if (PosOnFrame >= PosUBound) begin 
                         StartPos = StartPos + LEN + 1'd1; 
                         if (AddrCount < 18'd179995) AddrCount = AddrCount + 1'd1;//after adding max will be 179995
                         else AddrCount = 18'b0;
